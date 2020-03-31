0eaa95423d1db2a6ebfaf5929ed0db25a99b35e4
Eric <32319172+easXin@users.noreply.github.com>
2020-03-30 23:20:05 -0700

    Update app.js

--- ng-spa/app.js
+++ ng-spa/app.js
@@ -7,4 +7,17 @@ angular.module('myApp',[])
                   }
                   return 100 - $scope.msg.length;
             }
+            $scope.save=function(){
+              // use json
+                alert('note is saved');
+                localStorage.setItem('note_key',JSON.stringify($scope.msg));
+                $scope.msg='';
+            }
+            $scope.read=function(){
+                $scope.msg=JSON.parse(localStorage.getItem('note_key')||[]);
+            }
+            $scope.remove=function(){
+                localStorage.removeItem('note_key');
+                $scope.msg='';
+            }
       }]);
